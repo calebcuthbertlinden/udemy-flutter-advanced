47ad60539983a67a9d850fff97e0aef357782a69
Caleb Linden <calebl@discovery.co.za>
2021-11-10 22:39:20 +0200

    Assignment 2 finished

--- flutter_app/lib/Animations.dart
+++ flutter_app/lib/Animations.dart
@@ -27,28 +27,71 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+class AnimatedLogo extends AnimatedWidget {
+
+  AnimatedLogo(Key key, Animation<double> animation) : super(key: key, listenable: animation);
+
+  static final opacityTween = Tween<double>(begin: 0.1, end: 1.0);
+  static final sizeTween = Tween<double>(begin: 0.0, end: 300.0);
+  static final rotateTween = Tween<double>(begin: 0.0, end: 12.0);
+
+  @override
+  Widget build(BuildContext context) {
+    final Animation<double> animation =  listenable as Animation<double>;
+    return Center(
+      child: Transform.rotate(angle: rotateTween.evaluate(animation),
+      child: Opacity(opacity: opacityTween.evaluate(animation),
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10.0),
+        height: sizeTween.evaluate(animation),
+        width: sizeTween.evaluate(animation),
+        child: FlutterLogo()
+      ),))
+    );
+  }
+
+}
+
 class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin{
 
-  Animation<double>? animation;
-  AnimationController? controller;
+  late Animation<double> animation;
+  late Animation<double> curvedAnimation;
+  late AnimationController controller;
 
   @override
   void initState() {
     super.initState();
     controller = AnimationController(duration: const Duration(milliseconds: 5000), vsync: this);
-    animation = new Tween(begin: 0.0, end: 400.0).animate(controller!);
-    animation!.addListener(() {
-      setState(() {
-        // todo
-      });
-    });
 
-    controller!.forward();
+    // Tween animation
+    // animation = new Tween(begin: 0.0, end: 400.0).animate(controller);
+
+    // Curve animation
+    curvedAnimation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
+    curvedAnimation.addStatusListener(listener);
+    controller.forward();
+  }
+
+  void listener(AnimationStatus status) {
+    if (status == AnimationStatus.completed) {
+      controller.reverse();
+    } else if (status == AnimationStatus.dismissed) {
+      controller.forward();
+    }
+  }
+
+  // Animated widget
+  Widget builder(BuildContext context, Widget? child) {
+    return Container(
+      height: animation.value,
+      width: animation.value,
+      child: FlutterLogo()
+    );
   }
 
   @override
   void dispose() {
-    controller!.dispose();
+    controller.dispose();
     super.dispose();
   }
 
@@ -60,11 +103,10 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
       ),
       body:
       Container(
-        height: animation!.value,
-        width: animation!.value,
-        padding: EdgeInsets.all(32),
+       padding: EdgeInsets.all(32.0),
         child: Center(
-          child: FlutterLogo(size: 300)
+          child: AnimatedLogo(Key("1"), curvedAnimation)
+          // child: AnimatedBuilder(animation: animation, builder: builder)
         )
       ),
     );
